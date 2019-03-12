d0956a3dbe4889b9ecce0257ee579a7b408eafb4
Gerwin <iamgerwin@aim.com>
2019-03-12 13:11:07 +0800

    abstract factory pattern

--- index.js
+++ index.js
@@ -5,23 +5,59 @@ class Car
         this.door = door;
         this.engine = engine;
         this.color = color;
-    
+        this.passenger = 4;
     }
 }
 
-class carFactory
+class Suv extends Car
 {
-    createCar(type) {
+    constructor(door, engine, color, passenger = 7)
+    {
+        super(door, engine, color);
+        this.passenger = passenger;
+    }
+}
+class SuvFactory
+{
+    createVehicle(type) {
         switch (type) {
-            case 'civic':
-                return new Car(2, 'V6', 'grey');
-            case 'vios':
-                return new Car(4, 'V4', 'red'); 
+            case 'dmax':
+                return new Suv(4, 'V8', 'pink');
+            case 'montero':
+                return new Suv(4, 'V12', 'fuchsia'); 
         }
     }
 }
 
-const factory = new carFactory();
-const car1 = factory.createCar('vios');
 
-console.log(car1);
+class CarFactory
+{
+    createVehicle(type) {
+        switch (type) {
+            case 'vios':
+                return new Car(4, 'V4', 'black');
+            case 'civic':
+                return new Car(2, 'V8', 'white'); 
+        }
+    }
+}
+
+const carFactory = new CarFactory();
+const suvFactory = new SuvFactory();
+
+const autoManufacturer = (type, model) => {
+    
+
+    switch (type) {
+        case 'car':
+            return carFactory.createVehicle(model);
+        case 'suv':
+            return suvFactory.createVehicle(model);
+    }
+}
+
+const vehicle1 = autoManufacturer('suv', 'montero');
+const vehicle2 = autoManufacturer('car', 'civic');
+
+console.log(vehicle1);
+console.log(vehicle2);
